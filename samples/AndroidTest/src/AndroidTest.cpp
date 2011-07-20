34e50aa68478dbcc5421d81ece3d186c471854d4
Dan V <dan@expandingbrain.com>
2011-07-20 12:16:48 +0800

    Testing Android resource stream reading.

--- samples/AndroidTest/src/AndroidTest.cpp
+++ samples/AndroidTest/src/AndroidTest.cpp
@@ -1,6 +1,9 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/Rand.h"
 
+#include "cinder/DataSource.h"
+#include "cinder/Stream.h"
+
 using namespace ci;
 using namespace ci::app;
 
@@ -15,6 +18,20 @@ public:
 void AndroidTest::setup()
 {
 	console() << "AndroidTest" << endl;
+	DataSourceRef data = loadResource("hello.txt");
+	console() << "Loaded hello.txt" << endl;
+	IStreamRef stream = data->createStream();
+	int size = stream->size();
+	console() << "Stream size: " << size << endl;
+	char* input = new char[size+1];
+	int read = stream->readDataAvailable(input, 1024);
+	input[read] = '\0';
+	console() << "Read " << read << " bytes from stream" << endl;
+	console() << "value: " << input << endl;
+
+	// string text;
+	// stream->readFixedString(&text, size);
+	// console() << "Read from hello.txt: " << text << endl;
 }
 
 void AndroidTest::draw()
