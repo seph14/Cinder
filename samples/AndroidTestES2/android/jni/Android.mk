98357700c094ffef2c9a966822f00f2044b6ad3c
Dan V <dan@expandingbrain.com>
2011-08-02 15:55:58 +0800

    Added ES2 test sample

---
+++ samples/AndroidTestES2/android/jni/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+CINDER_PATH        := $(LOCAL_PATH)/../../../..
+CINDER_MODULE_PATH := ../../../../android/obj/local/armeabi
+
+include $(CINDER_PATH)/android/CinderModules.mk
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := AndroidTestES2
+LOCAL_C_INCLUDES := $(CINDER_PATH)/include \
+					$(CINDER_PATH)/boost
+
+
+LOCAL_SRC_FILES := ../../src/AndroidTestES2.cpp
+
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -lz
+LOCAL_STATIC_LIBRARIES := cinder freeimage ft2 android_native_app_glue 
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
+
