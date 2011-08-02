0778ba6c8ba1909da3d502eb23bebde8961a8939
Dan V <dan@expandingbrain.com>
2011-08-02 15:34:18 +0800

    Consistent tab usage in Cinder.mk

--- android/jni/Cinder.mk
+++ android/jni/Cinder.mk
@@ -20,10 +20,8 @@ LOCAL_SRC_FILES  := $(CINDER_SRC)/app/App.cpp \
 					$(CINDER_SRC)/app/AppImplAndroidRendererGl.cpp \
 					$(CINDER_SRC)/gl/gl.cpp \
 					$(CINDER_SRC)/gl/Fbo.cpp \
-					$(CINDER_SRC)/gl/Light.cpp \
-					$(CINDER_SRC)/gl/Material.cpp \
 					$(CINDER_SRC)/gl/Texture.cpp \
-					$(CINDER_SRC)/gl/TileRender.cpp \
+					$(CINDER_SRC)/gl/Vbo.cpp \
 					$(CINDER_SRC)/ip/Blend.cpp \
 					$(CINDER_SRC)/ip/EdgeDetect.cpp \
 					$(CINDER_SRC)/ip/Fill.cpp \
@@ -35,39 +33,39 @@ LOCAL_SRC_FILES  := $(CINDER_SRC)/app/App.cpp \
 					$(CINDER_SRC)/ip/Threshold.cpp \
 					$(CINDER_SRC)/ip/Trim.cpp \
 					$(CINDER_SRC)/Area.cpp \
-                    $(CINDER_SRC)/AxisAlignedBox.cpp \
-                    $(CINDER_SRC)/BandedMatrix.cpp \
-                    $(CINDER_SRC)/BSpline.cpp \
-                    $(CINDER_SRC)/BSplineFit.cpp \
-                    $(CINDER_SRC)/Buffer.cpp \
-                    $(CINDER_SRC)/Camera.cpp \
-                    $(CINDER_SRC)/Channel.cpp \
-                    $(CINDER_SRC)/Color.cpp \
-                    $(CINDER_SRC)/DataSource.cpp \
-                    $(CINDER_SRC)/DataTarget.cpp \
-                    $(CINDER_SRC)/Exception.cpp \
+					$(CINDER_SRC)/AxisAlignedBox.cpp \
+					$(CINDER_SRC)/BandedMatrix.cpp \
+					$(CINDER_SRC)/BSpline.cpp \
+					$(CINDER_SRC)/BSplineFit.cpp \
+					$(CINDER_SRC)/Buffer.cpp \
+					$(CINDER_SRC)/Camera.cpp \
+					$(CINDER_SRC)/Channel.cpp \
+					$(CINDER_SRC)/Color.cpp \
+					$(CINDER_SRC)/DataSource.cpp \
+					$(CINDER_SRC)/DataTarget.cpp \
+					$(CINDER_SRC)/Exception.cpp \
 					$(CINDER_SRC)/Font.cpp \
-                    $(CINDER_SRC)/ImageIo.cpp \
-                    $(CINDER_SRC)/ImageSourceFileFreeImage.cpp \
-                    $(CINDER_SRC)/Matrix.cpp \
-                    $(CINDER_SRC)/Path2D.cpp \
-                    $(CINDER_SRC)/Perlin.cpp \
-                    $(CINDER_SRC)/PolyLine.cpp \
-                    $(CINDER_SRC)/Rand.cpp \
-                    $(CINDER_SRC)/Ray.cpp \
-                    $(CINDER_SRC)/Rect.cpp \
-                    $(CINDER_SRC)/Shape2D.cpp \
-                    $(CINDER_SRC)/Sphere.cpp \
-                    $(CINDER_SRC)/Stream.cpp \
-                    $(CINDER_SRC)/Surface.cpp \
-                    $(CINDER_SRC)/System.cpp \
-                    $(CINDER_SRC)/Timer.cpp \
-                    $(CINDER_SRC)/Triangulate.cpp \
-                    $(CINDER_SRC)/TriMesh.cpp \
-                    $(CINDER_SRC)/Url.cpp \
-                    $(CINDER_SRC)/UrlImplAndroid.cpp \
-                    $(CINDER_SRC)/Utilities.cpp \
-                    $(CINDER_SRC)/Xml.cpp \
+					$(CINDER_SRC)/ImageIo.cpp \
+					$(CINDER_SRC)/ImageSourceFileFreeImage.cpp \
+					$(CINDER_SRC)/Matrix.cpp \
+					$(CINDER_SRC)/Path2D.cpp \
+					$(CINDER_SRC)/Perlin.cpp \
+					$(CINDER_SRC)/PolyLine.cpp \
+					$(CINDER_SRC)/Rand.cpp \
+					$(CINDER_SRC)/Ray.cpp \
+					$(CINDER_SRC)/Rect.cpp \
+					$(CINDER_SRC)/Shape2D.cpp \
+					$(CINDER_SRC)/Sphere.cpp \
+					$(CINDER_SRC)/Stream.cpp \
+					$(CINDER_SRC)/Surface.cpp \
+					$(CINDER_SRC)/System.cpp \
+					$(CINDER_SRC)/Timer.cpp \
+					$(CINDER_SRC)/Triangulate.cpp \
+					$(CINDER_SRC)/TriMesh.cpp \
+					$(CINDER_SRC)/Url.cpp \
+					$(CINDER_SRC)/UrlImplAndroid.cpp \
+					$(CINDER_SRC)/Utilities.cpp \
+					$(CINDER_SRC)/Xml.cpp \
 					$(TESS_SRC)/bucketalloc.c \
 					$(TESS_SRC)/dict.c \
 					$(TESS_SRC)/geom.c \
@@ -76,7 +74,7 @@ LOCAL_SRC_FILES  := $(CINDER_SRC)/app/App.cpp \
 					$(TESS_SRC)/sweep.c \
 					$(TESS_SRC)/tess.c
 
-LOCAL_LDLIBS     		:= -lz -lGLESv1_CM -landroid -llog -lEGL
+LOCAL_LDLIBS				:= -lz -lGLESv1_CM -landroid -llog -lEGL
 LOCAL_STATIC_LIBRARIES	:= android_native_app_glue 
 
 include $(BUILD_STATIC_LIBRARY)
