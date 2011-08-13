db57c44c00c53936af33534bdb6c6932f5117ccc
Dan V <dan@expandingbrain.com>
2011-08-13 16:39:30 +0800

    OSX build fixes

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,14 +1,4 @@
 TOP_PATH := $(call my-dir)
 
-# include $(TOP_PATH)/cinder/Configure.mk
-
-# ifdef USE_FREEIMAGE
-# include $(TOP_PATH)/FreeImage/Android.mk
-# endif
-# 
-# ifdef USE_FREETYPE
-# include $(TOP_PATH)/freetype-2.4.5/Android.mk
-# endif
-
 include $(TOP_PATH)/cinder/Android.mk
 
