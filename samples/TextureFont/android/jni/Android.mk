f8190febd41b2bc987fc3b751ef5f87ab4597547
dan <dan@expandingbrain.com>
2011-08-17 12:43:54 +0800

    TextureFont sample makefiles

---
+++ samples/TextureFont/android/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := TextureFont
+LOCAL_SRC_FILES := ../../src/TextureFontApp.cpp
+
+LOCAL_LDLIBS    := -landroid 
+LOCAL_STATIC_LIBRARIES := cinder freeimage ft2 android_native_app_glue 
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
+$(call import-module,cinder)
+
