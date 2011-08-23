bfb920b98e8bebbe3c5ff84fc1d7c96e11b5d6ff
Dan V <dan@expandingbrain.com>
2011-08-23 23:38:54 +0800

    Testing 3D drawing methods

--- samples/AndroidTestES2/src/AndroidTestES2.cpp
+++ samples/AndroidTestES2/src/AndroidTestES2.cpp
@@ -9,6 +9,7 @@
 #include "cinder/gl/gles2.h"
 
 #include "cinder/Font.h"
+#include "cinder/Camera.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -24,6 +25,7 @@ public:
     GlesContextRef mContext;
 	Texture	       mTexture;	
 	Font mFont;
+    CameraPersp    mCam;
 };
 
 void AndroidTestES2::setup()
@@ -44,28 +46,44 @@ void AndroidTestES2::setup()
 	mFont = Font(fontData, 16);
 	console() << "Loaded font name " << mFont.getName() << " num glyphs " << mFont.getNumGlyphs() << endl;
 
-    setMatricesWindow(getWindowWidth(), getWindowHeight());
-    glDisable(GL_CULL_FACE);
+    float aspectRatio = float(getWindowWidth()) / float(getWindowHeight());
+    mCam.setPerspective(50.0f, aspectRatio, 0.1f, 1000.0f);
+    mCam.setEyePoint(Vec3f(0, 0, 10.0f));
+    mCam.lookAt(Vec3f(0, 0, 0));
+
+    glEnable(GL_CULL_FACE);
 }
 
 void AndroidTestES2::draw()
 {
     mContext->bind();
 
+    setMatricesWindow(getWindowWidth(), getWindowHeight());
 	clear( Color( 0.2f, 0.2f, 0.2f ) );
 
     color(ColorA(1.0f, 1.0f, 0, 1.0f));
     drawLine(Vec2f(0, 0), Vec2f(200, 200));
 
     color(ColorA(1.0f, 0, 0, 1.0f));
-    drawSolidCircle( Vec2f(getWindowWidth() / 2.0f, getWindowHeight() / 2.0f), 
-                                     40.0f, 32);
+    // drawSolidCircle( Vec2f(getWindowWidth() / 2.0f, getWindowHeight() / 2.0f), 
+    //                                  40.0f, 32);
 
     if (mTexture) {
         color(ColorA(1.0f, 1.0f, 1.0f, 1.0f));
         Vec2f lr(getWindowWidth(), getWindowHeight());
         gl::draw(mTexture, Rectf(lr.x-100.0f, lr.y-100.0f, lr.x, lr.y));
     }
+
+    mTexture.unbind();
+    color(ColorA(1.0f, 0.1f, 0.1f, 0.8f));
+    setMatrices(mCam);
+    rotate(Vec3f(15.0f, 15.0f, 15.0f));
+    // drawColorCube( Vec3f(0, 0, 0), Vec3f(3.0f, 3.0f, 3.0f) );
+    drawStrokedCube( Vec3f(0, 0, 0), Vec3f(3.0f, 3.0f, 3.0f) );
+    pushModelView();
+    color(ColorA(0.2f, 0.2f, 1.0f, 1.0f));
+    rotate(Vec3f(15.0f, 15.0f, 15.0f));
+    drawStrokedCube( Vec3f(0, 0, 0), Vec3f(3.0f, 3.0f, 3.0f) );
 
     mContext->unbind();
 }
