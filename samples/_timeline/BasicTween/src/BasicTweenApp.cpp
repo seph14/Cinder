117f95745b3304257b6a12d330b5d2d3dccd31f6
Dan V <dan@expandingbrain.com>
2011-08-29 17:16:25 +0800

    BasicTween sample

--- samples/_timeline/BasicTween/src/BasicTweenApp.cpp
+++ samples/_timeline/BasicTween/src/BasicTweenApp.cpp
@@ -1,11 +1,19 @@
-#include "cinder/app/AppBasic.h"
+#include "cinder/app/AppNative.h"
+
+#if defined( CINDER_GLES2 )
+#include "cinder/gl/gles2.h"
+#endif
 
 using namespace std;
 using namespace ci;
 using namespace ci::app;
 
-class BasicTweenApp : public AppBasic {
+class BasicTweenApp : public AppNative {
+#if defined( CINDER_GLES2 )
+    gl::GlesContextRef mContext;
+#endif
   public:
+    void prepareSettings(Settings *settings);
 	void setup();
 	void mouseDown( MouseEvent event );
 	void draw();
@@ -13,8 +21,17 @@ class BasicTweenApp : public AppBasic {
 	Vec2f mBlackPos, mWhitePos;
 };
 
+void BasicTweenApp::prepareSettings(Settings *settings)
+{	
+    settings->enableMultiTouch(false);
+}
+
 void BasicTweenApp::setup()
 {	
+#if defined( CINDER_GLES2 )
+    mContext = gl::setGlesContext();
+    gl::setMatricesWindow(getWindowWidth(), getWindowHeight());
+#endif
 	mBlackPos = mWhitePos = getWindowCenter();
 }
 
@@ -28,6 +45,10 @@ void BasicTweenApp::mouseDown( MouseEvent event )
 
 void BasicTweenApp::draw()
 {
+#if defined( CINDER_GLES2 )
+    mContext->bind();
+#endif
+
 	gl::clear( Color( 0.5f, 0.5f, 0.5f ) );
 	
 	gl::color( Color::black() );
@@ -35,7 +56,11 @@ void BasicTweenApp::draw()
 	
 	gl::color( Color::white() );
 	gl::drawSolidCircle( mWhitePos, 16.0f );
+
+#if defined( CINDER_GLES2 )
+    mContext->unbind();
+#endif    
 }
 
 // This line tells Cinder to actually create the application
-CINDER_APP_BASIC( BasicTweenApp, RendererGl )
+CINDER_APP_NATIVE( BasicTweenApp, RendererGl )
