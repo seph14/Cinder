7228ecc7ea142a5a9c0add7782b9cc9e79fc06bf
Dan V <dan@expandingbrain.com>
2012-02-23 23:01:14 +0800

    Split out FreeImage and FreeType modules from Cinder makefile

--- android/setup/freetype/Android.mk
+++ android/setup/freetype/Android.mk
@@ -1,35 +1,40 @@
 # based on Android.mk from Android source distribution
 #
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
+
+FREETYPE_PATH = $(TOP_PATH)/freetype-2.4.5
 
 # compile in ARM mode, since the glyph loader/renderer is a hotspot
 # when loading complex pages in the browser
 #
 LOCAL_ARM_MODE := arm
 
-LOCAL_SRC_FILES:= \
-	src/base/ftbbox.c \
-	src/base/ftbitmap.c \
-	src/base/ftglyph.c \
-	src/base/ftstroke.c \
-	src/base/ftxf86.c \
-	src/base/ftbase.c \
-	src/base/ftsystem.c \
-	src/base/ftinit.c \
-	src/base/ftgasp.c \
-	src/raster/raster.c \
-	src/sfnt/sfnt.c \
-	src/smooth/smooth.c \
-	src/autofit/autofit.c \
-	src/truetype/truetype.c \
-	src/cff/cff.c \
-	src/psnames/psnames.c \
-	src/pshinter/pshinter.c
+FREETYPE_SRC = ../freetype-2.4.5/src
+
+LOCAL_SRC_FILES := \
+	$(FREETYPE_SRC)/base/ftbbox.c \
+	$(FREETYPE_SRC)/base/ftbitmap.c \
+	$(FREETYPE_SRC)/base/ftglyph.c \
+	$(FREETYPE_SRC)/base/ftstroke.c \
+	$(FREETYPE_SRC)/base/ftxf86.c \
+	$(FREETYPE_SRC)/base/ftbase.c \
+	$(FREETYPE_SRC)/base/ftsystem.c \
+	$(FREETYPE_SRC)/base/ftinit.c \
+	$(FREETYPE_SRC)/base/ftgasp.c \
+	$(FREETYPE_SRC)/raster/raster.c \
+	$(FREETYPE_SRC)/sfnt/sfnt.c \
+	$(FREETYPE_SRC)/smooth/smooth.c \
+	$(FREETYPE_SRC)/autofit/autofit.c \
+	$(FREETYPE_SRC)/truetype/truetype.c \
+	$(FREETYPE_SRC)/cff/cff.c \
+	$(FREETYPE_SRC)/psnames/psnames.c \
+	$(FREETYPE_SRC)/pshinter/pshinter.c
 
 LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/builds \
-	$(LOCAL_PATH)/include
+	$(FREETYPE_PATH)/builds \
+	$(FREETYPE_PATH)/include
 
 LOCAL_CFLAGS += -W -Wall
 LOCAL_CFLAGS += -fPIC -DPIC
@@ -38,7 +43,7 @@ LOCAL_CFLAGS += "-DFT2_BUILD_LIBRARY"
 
 LOCAL_CFLAGS += -O2
 
-LOCAL_MODULE:= libft2
+LOCAL_MODULE:= ft2
 
 include $(BUILD_STATIC_LIBRARY)
 
