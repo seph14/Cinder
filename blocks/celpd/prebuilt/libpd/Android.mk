5919324993708c1b5535b748f6534b1d9f361d45
Dan V <dan@expandingbrain.com>
2012-03-16 10:08:40 +0800

    Renamed prebuilt module source files

--- blocks/celpd/prebuilt/libpd/Android.mk
+++ blocks/celpd/prebuilt/libpd/Android.mk
@@ -8,49 +8,49 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := choice
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libchoice.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/choice.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := bonk~
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libbonk~.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/bonk~.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := lrshift~
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/liblrshift~.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/lrshift~.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fiddle~
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libfiddle~.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/fiddle~.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := sigmund~
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libsigmund~.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/sigmund~.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pique
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libpique.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/pique.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := loop~
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libloop~.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/loop~.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := expr
 LOCAL_CFLAGS := -DPD
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libexpr.so
+LOCAL_SRC_FILES := $(TARGET_ARCH)/expr.pd_linux
 include $(PREBUILT_SHARED_LIBRARY)
 
 
