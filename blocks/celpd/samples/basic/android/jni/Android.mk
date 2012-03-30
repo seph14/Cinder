f82b5fe573e29393892aee6dd17079e33e187a5b
Dan V <dan@expandingbrain.com>
2012-03-30 17:28:22 +0800

    Import libpdnative without linking.

--- blocks/celpd/samples/basic/android/jni/Android.mk
+++ blocks/celpd/samples/basic/android/jni/Android.mk
@@ -11,9 +11,9 @@ LOCAL_SRC_FILES := ../../src/basicAudioApp.cpp \
 				  $(CELAUDIO_SRC)/libpd_dl.c \
 				  $(CELAUDIO_SRC)/opensl.cpp
 
-LOCAL_LDLIBS    := -landroid -lOpenSLES
+LOCAL_LDFLAGS := -Wl,--as-needed
+LOCAL_LDLIBS  := -landroid -lOpenSLES
 LOCAL_STATIC_LIBRARIES := cinder boost_system boost_filesystem boost_thread freeimage ft2 android_native_app_glue 
-LOCAL_SHARED_LIBRARIES := pdnative 
 
 include $(BUILD_SHARED_LIBRARY)
 
--- blocks/celpd/samples/basic/android/jni/Application.mk
+++ blocks/celpd/samples/basic/android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_PLATFORM := android-9
 APP_STL      := gnustl_static
-APP_MODULES  := BasicAudio
+APP_MODULES  := BasicAudio pdnative
 APP_CPPFLAGS := -fexceptions -frtti
 
